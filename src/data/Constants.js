28723e85f16eccc2c504413072f99cc01219011f
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-01-30 16:02:56 +0530

    constant value

---
+++ src/data/Constants.js
@@ -0,0 +1,4 @@
+export const SUBMIT_BUTTON_LABEL = 'Submit';
+export const FORM_LABEL_EMAIL = 'email';
+export const FORM_LABEL_PASSWORD = 'Password';
+export const FORM_LABEL_CONFIRM_PASSWORD = 'Confirm password';
