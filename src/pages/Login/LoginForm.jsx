0205cb2cf04c8ce89234dd3fcba1744e8586be66
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-04-06 14:51:08 +0530

    Login and Signup form

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -1,42 +1,78 @@
-import React, { useState } from 'react';
-import Button from '../../components/Form/Button';
-import Inputs from '../../components/Form/Inputs';
-import { FORM_LABEL_EMAIL , FORM_LABEL_PASSWORD } from '../../data/Constants';
+import { Link } from 'react-router-dom';
+import { Button } from "@/components/ui/button"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import AuthContext from "@/context/AuthContext";
+import { useContext, useState } from "react";
 
 function LoginForm() {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const { login } = useContext(AuthContext);
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(`Name: ${name}, Email: ${email}`);
+	};
+	const handleButtonClick = () => {
+		login()
+		console.log('Submit button clicked!');
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Name: ${name}, Email: ${email}`);
-  };
-  const handleButtonClick = () => {
-    console.log('Submit button clicked!');
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <Inputs
-          placeholder={FORM_LABEL_EMAIL }
-          type={"email"}
-          value={name}
-          onChange={(event) => setName(event.target.value)} />
-      </label>
-      <label>
-        Password:
-        <Inputs
-          placeholder={FORM_LABEL_PASSWORD}
-          type={FORM_LABEL_PASSWORD}
-          value={FORM_LABEL_PASSWORD}
-          onChange={(event) => setEmail(event.target.value)} />
-      </label>
-      <Button label="Submit" onClick={handleButtonClick} />
-    </form>
-  );
+	return (
+		
+			<Card className="mx-auto max-w-sm mt-20">
+				<CardHeader>
+					<CardTitle className="text-2xl">Login</CardTitle>
+					<CardDescription>
+						Enter your email below to login to your account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<div className="grid gap-4">
+						<div className="grid gap-2">
+							<Label htmlFor="email">Email</Label>
+							<Input
+								id="email"
+								type="email"
+								placeholder="m@example.com"
+								required
+							/>
+						</div>
+						<div className="grid gap-2">
+							<div className="flex items-center">
+								<Label htmlFor="password">Password</Label>
+								<button href="#" className="ml-auto inline-block text-sm underline">
+									Forgot your password?
+								</button>
+							</div>
+							<Input id="password" type="password" required />
+						</div>
+						<Button type="submit" onClick={handleButtonClick} className="w-full">
+							Login
+						</Button>
+						<Button variant="outline" className="w-full">
+							Login with Google
+						</Button>
+					</div>
+					<div className="mt-4 text-center text-sm">
+						Don&apos;t have an account?{" "}
+						{/* <button href="#" onClick={OnSignUp} >
+							Sign up
+						</button> */}
+						<Link className="underline" to="/signup">Sign Up</Link>
+						
+					</div>
+				</CardContent>
+			</Card>
+		
+	)
 }
-
 export default LoginForm;
