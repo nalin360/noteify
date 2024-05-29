14d01ebc2e830b3dad890d48c16f4596275a1e3e
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-05-29 22:36:43 +0530

    updated useNotes hoocks

--- src/hooks/useNotes.jsx
+++ src/hooks/useNotes.jsx
@@ -1,23 +1,78 @@
 // src/useNotes.js
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { createDocuments, createNotesDB, deleteDB, updateDB } from '@/config/dbFirestore';
+import { auth, db } from '@/config/firebase';
 
-const useNotes = (initialNotes = [{ id: 'note1', text: '' }]) => {
- const [notes, setNotes] = useState(initialNotes);
+const useNotes =  (initialNotes = [{
+	data: {
+		id: 1,
+		text: {
+			html: "",
+			markdown: ""
+		}
+	}
+}]) => {
+	const [notes, setNotes] = useState(initialNotes);
 
- const addNote = () => {
-    const newNoteId = `note${notes.length + 1}`;
-    setNotes([...notes, { id: newNoteId, text: '' }]);
- };
+	const userId =  auth.currentUser;
 
- const updateNoteText = (id, text) => {
-    setNotes(notes.map((note) => (note.id === id ? { ...note, text } : note)));
- };
+	// console.log(userId.uid);
+	
+	createDocuments(userId.uid);
+	const userIduid = userId.uid;
+	const path = userIduid ? `/users/${userId.uid}/notes` : null;
 
- const deleteNote = (id) => {
-    setNotes(notes.filter((note) => note.id !== id));
- };
 
- return { notes, addNote, updateNoteText, deleteNote };
+
+	useEffect(() => {
+		const unsubscribe = onSnapshot(collection(db, path), (snapshot) => {
+			const notesData = snapshot.docs.map((doc) => ({
+
+				id: doc.id,
+				data: {
+					...doc.data()
+				},
+
+
+			}));
+			setNotes(notesData);
+		});
+
+		console.log(notes);
+
+		return () => unsubscribe();
+	}, [userId]);
+
+
+	const addNote = async () => {
+		const newNoteId = notes.length + 1/* the ID returned by createNotesDB */
+		// Firestore will automatically generate a unique ID for the new document
+		console.log("add", notes);
+		await createNotesDB(path, "", newNoteId);
+		// Assuming createNotesDB returns the new note's ID
+		setNotes([...notes, { id: newNoteId, text }]);
+	};
+
+
+	const updateNoteText = async (id, text) => {
+		const noteToUpdate = notes.find(note => note.id === id);
+		console.log(id);
+		if (noteToUpdate) {
+			await updateDB(noteToUpdate.id, text, `${path}/${id}`);
+			setNotes(notes.map(note => note.id === id ? { ...note, text } : note));
+		}
+	};
+
+
+	const deleteNote = async (id) => {
+		await deleteDB(path, id);
+		setNotes(notes.filter(note => note.id !== id));
+	};
+
+
+	return { notes, addNote, updateNoteText, deleteNote };
 };
 
 export default useNotes;
+
